4aa4ec8a38e8f9e43707abb24dc0d4759b3720c5
Dell Home <pon.felipon@gmail.com>
2019-07-12 07:25:51 -0700

    Agegando comentarios a los archivos de configuracion

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Variables to configure the emal notification service
+    # la mayoria de estas variables las encuentras en el archivo
+    # .env para ser ejecutadas por pydotenv
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = os.environ.get('MAIL_PORT' or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
