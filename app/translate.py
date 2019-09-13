f095b2eb4b1f7b3947654571b29b81e3c9cf7e49
Deepin pc <felipe@nogson.com>
2019-09-13 02:03:21 -0700

    funcionando api Translate de Azure

--- app/translate.py
+++ app/translate.py
@@ -4,20 +4,24 @@ from flask_babel import _
 from app import app
 
 #borrar esto es por mientras
-end_point = 'https://blog-translator.cognitiveservices.azure.com/sts/v1.0/issuetoken'
-key = '5888b2991cca4fc5abcb71acb719f34a'
-def translate(text, source_language, dest_language):
+def translate(text,  dest_language, source_language=None):
     if 'MS_TRANSLATOR_KEY' not in app.config or \
         not app.config['MS_TRANSLATOR_KEY']:
         return _('Error: The tranlsation server is not configured')
-    auth = {'0cp-Apim-Subscription-Key': app.config['MS_TRANSLATOR_KEY']}
-    headers = {'0cp-Apim-Subscription-Key' : app.config['MS_TRANSLATOR_KEY'],
-            'Content-type' : 'aplication/json'}
+
+    headers = {'Ocp-Apim-Subscription-Key' : app.config['MS_TRANSLATOR_KEY'], 
+        'Content-type' : 'application/json'}
+
     body = [{
         'text' : text
     }]
+    
+    end_point = app.config['MS_TRANSLATOR_ENDPOINT'] 
     path = '/translate?api-version=3.0'
-    params = '&to={source_language}&to={dest_language}'
+    params = f'&to={dest_language}&from{source_language}'
     constructed_url = end_point + path + params
+    print(constructed_url)
+    print(headers)
+
     r = requests.post(constructed_url, headers=headers, json=body)
-    return r.json()
+    return r
